f87b0acef97b141989006e7a8b32a63b2a7d20e5
iwipwq <iwipwq@gmail.com>
2022-03-29 17:27:41 +0900

    add header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
             <span className="header-title-small">React & Node</span>
             <span className="header-title-large">Blog</span>
         </div>
-        <img src="" alt="" className="header-img" />
+        <img src="https://images.unsplash.com/photo-1648210403155-612156b36bde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2029&q=80" alt="타이틀이미지" className="header-img" />
     </div>
   )
 }
