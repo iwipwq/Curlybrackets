9cf0d97ee76db932177eaaf1a95b22f1040260fc
iwipwq <iwipwq@gmail.com>
2022-03-29 16:23:05 +0900

    add topbar

---
+++ src/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+        <div className="header-titles">
+            <span className="header-title-small">React & Node</span>
+            <span className="header-title-large">Blog</span>
+        </div>
+        <img src="" alt="" className="header-img" />
+    </div>
+  )
+}
---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+import "./topbar.css"
+
+export default function TopBar() {
+  return (
+    <div className="top">
+        <div className="top-left">
+            <i className="top-icon fa-brands fa-github-square"></i>
+            <i className="top-icon fa-brands fa-instagram-square"></i>
+        </div>
+        <div className="top-center">
+            <ul className="top-list">
+                <li className="top-list-item">HOME</li>
+                <li className="top-list-item">ABOUT</li>
+                <li className="top-list-item">CONTACT</li>
+                <li className="top-list-item">WRITE</li>
+                <li className="top-list-item">LOGOUT</li>
+            </ul>
+        </div>
+        <div className="top-right">
+            <img className="top-img" src="" alt="내 프로필 이미지" />
+            <i className="top-search-icon fas fa-search"></i>
+        </div>
+    </div>
+  )
+}
