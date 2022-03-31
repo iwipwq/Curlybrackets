572407343953af4bdd465beefb737dc525d18b26
iwipwq <iwipwq@gmail.com>
2022-03-31 21:28:51 +0900

    route setting if user login or not

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
       <TopBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={ user ? <Home /> : <Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/write" element={<Write />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route path="/register" element={ user ? <Home /> : <Register /> } />
+        <Route path="/login" element={ user ? <Home /> : <Login /> } />
+        <Route path="/write" element={ user ? <Write /> : <Home /> } />
+        <Route path="/settings" element={ user ? <Settings /> : <Home /> } />
         <Route path="/post/:postId" element={<Single />} />
       </Routes>
     </BrowserRouter>
