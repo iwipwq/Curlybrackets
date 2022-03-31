a87cebd1a4d3f7659cf78ae4aa4ca5133529a2f7
iwipwq <iwipwq@gmail.com>
2022-03-31 20:36:55 +0900

    add login & register page

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.scss"
+
+export default function Login() {
+  return (
+    <div className="login">
+        <span className="login-title">환영합니다.</span>
+        <form className="login-form">
+            <label>Email</label>
+            <input type="text" placeholder="이메일을 입력해주세요 ..." className="login-input" />
+            <label>Password</label>
+            <input type="password" placeholder="비밀번호를 입력해주세요 ..." className="login-input"/>
+            <button className="login-button">로그인</button>
+        </form>
+        <button className="login-register-button">회원가입</button>
+    </div>
+  )
+}
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.scss"
+
+export default function Register() {
+  return (
+    <div className="register">
+        <span className="register-title">환영합니다.</span>
+        <form className="register-form">
+            <label>User name</label>
+            <input type="text" placeholder="이름을 입력해주세요 ..." className="register-input" />
+            <label>Email</label>
+            <input type="text" placeholder="이메일을 입력해주세요 ..." className="register-input" />
+            <label>Password</label>
+            <input type="password" placeholder="비밀번호를 입력해주세요 ..." className="register-input"/>
+            <button className="register-button">회원가입</button>
+        </form>
+        <button className="register-login-button">로그인</button>
+    </div>
+  )
+}
