c35946ed1ac21e15fbb0fe1af568f8bfeae39fb9
iwipwq <iwipwq@gmail.com>
2022-04-01 23:52:52 +0900

    add login router, validator, response without password

--- api/index.js
+++ api/index.js
@@ -5,24 +5,16 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 
-dotenv.config()
+dotenv.config();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("몽고DB연결됨"))
 .catch((err) => console.log(err));
 
-app.use('/api/auth', authRoute)
+app.use('/api/auth', authRoute);
 
 console.log('5252');
-
-app.use("/gogo", (req, res) => {
-    console.log("gogo url")
-})
-
-app.get('/', (req, res) => {
-    res.send('안녕하세용')
-});
 
 app.listen(port, () => {
     console.log("success!")
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
-const { json } = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //REGISTER
-router.post("/register", async (req,res) => {
+router.post("/register", async (req, res) => {
     try {
 
         const saltRounds = 10;
@@ -25,5 +24,19 @@ router.post("/register", async (req,res) => {
 })
 
 //LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("아이디 혹은 비밀번호가 틀렸습니다.(사실 아이디가 틀림)");
+
+        const validated = await bcrypt.compare(req.body.password, user.password);
+        !validated && res.status(400).json("아이디 혹은 비밀번호가 틀렸습니다.(사실 비밀번호가 틀림)");
+
+        const { password, ...나머지정보들 } = user._doc;
+        res.status(200).json(나머지정보들);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
