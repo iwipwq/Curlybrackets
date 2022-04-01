247d6daebc3985b26860987e8476c0a1268bc508
iwipwq <iwipwq@gmail.com>
2022-04-01 21:09:58 +0900

    auth routes setting

--- api/index.js
+++ api/index.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 5000
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config()
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("몽고DB연결됨"))
 .catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute)
 
 console.log('5252');
 
--- api/models/User.js
+++ api/models/User.js
@@ -4,12 +4,12 @@ const UserSchema = new mongoose.Schema({
         username:{
             type: String,
             required: true,
-            unique: ture
+            unique: true
         },
         email:{
             type: String,
             required: true,
-            unique: ture
+            unique: true
         },
         password:{
             type: String,
