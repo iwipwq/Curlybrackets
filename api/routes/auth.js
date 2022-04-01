d1e191f662221cf62d82d656a9c1b5cb5320c625
iwipwq <iwipwq@gmail.com>
2022-04-01 22:20:57 +0900

    password encrypt with bcrypt

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,20 @@
 const router = require("express").Router();
 const { json } = require("express");
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //REGISTER
 router.post("/register", async (req,res) => {
     try {
+
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPass,
         })
 
         const user = await newUser.save();
