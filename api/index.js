ec35d33856e723e64ac526beaaa38ed34afd1773
iwipwq <iwipwq@gmail.com>
2022-04-03 23:18:07 +0900

    add category router/ multer setting

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -14,6 +15,19 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("몽고DB연결됨"))
 .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null, "images")
+    },filename:(req,file,cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"),(req,res) => {
+    res.status(200).json("파일이 업로드 되었습니다.")
+})
 
 app.use('/api/auth', authRoute);
 app.use('/api/user', userRoute);
