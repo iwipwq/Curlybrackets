e744279cbacdc50fcd138518689fe490eeb7b759
iwipwq <iwipwq@gmail.com>
2022-04-14 13:15:56 +0900

    update validate when userInput change

--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import Sidebar from "../../components/sidebar/Sidebar"
 import { Context } from "../../context/Context"
 import "./settings.scss"
@@ -10,7 +10,8 @@ export default function Settings() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [bio,setBio] = useState("")
-  const [errors,setErrors] = useState({});
+  const [errors,setErrors] = useState({name:"init"});
+  const [valid,setValid] = useState({name:false,message:"init"});
   const [success, setSuccess] = useState(false);
   const { user, dispatch } = useContext(Context);
   const [file, setFile] = useState(null);
@@ -20,124 +21,158 @@ export default function Settings() {
   } else if (!user.profileImg) {
     console.log("image nono")
   }
+  console.log(valid.name,errors);
+  const validateUsername = (username) => {
+    const isLengthValid = /^[A-Za-z\d@$!%*?&]{1,12}$/.test(username);
+    const isLetterValid = /^[a-zA-Z\u3131-\u318E\uAC00-\uD7A3\d]*$/.test(username);
+    switch (false) {
+      case isLengthValid:
+        console.log("lengthvalidfailed")
+        valid["name"] = false;
+        errors["name"] = "이름은 1자 이상 12자 이하로 해주세요";
+        break;
+      
+      case isLetterValid:
+        valid["name"] = false;
+        errors["name"] = "이름은 한글,영문,숫자만 사용가능합니다.";
+        break;
+    
+      default:
+        console.log("username valid");
+        valid["name"] = true;
+        valid["message"] = "사용가능한 닉네임입니다.";
+        console.log("valid값 변경",valid.name,valid["name"],valid);
+        break;
+    }
+  }
+
+  const validatePassword = (password) => {
+    const islengthValid = /^[A-Za-z\d@$!%*?&]{3,20}$/.test(password);
+    const atLeastOneUpperLowercaseLetter = /^(?=.*[a-z])(?=.*[A-Z])$/.test(password);
+    const atLeastOneNumber = /^(?=.*\d)$/.test(password);
+    const atLeastOneSpecialCharacter = /^(?=.*[@$!%*?&])$/.test(password);
+    switch (false) {
+      case islengthValid:
+        valid["password"] = false;
+        errors["password"] = "비밀번호는 3자 이상 20자 이하만 가능합니다."
+        break;
+      
+      case atLeastOneUpperLowercaseLetter:
+        valid["password"] = false;
+        errors["password"] = "영문 소문자,대문자가 각각 1개 이상 포함되어야 합니다."
+        break;
+      
+      case atLeastOneNumber:
+        valid["password"] = false;
+        errors["password"] = "숫자가 1개 이상 포함되어야 합니다."
+        break;
+      
+      case atLeastOneSpecialCharacter:
+        valid["password"] = false;
+        errors["password"] = "특수문자가 1개 이상 포함되어야 합니다."
+        break;
+    
+      default:
+        valid["password"] = true;
+        valid["message"] = "사용가능한 비밀번호입니다."
+        break;
+    }
+  }
+
+  const validateEmail = (email) => {
+    const isEmailFormat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    if(isEmailFormat) {
+      valid["email"] = true;
+      valid["message"] = "사용가능한 이메일형식입니다."
+    } else {
+      valid["email"] = false;
+      errors["email"] = "이메일 형식으로 입력해주세요"
+    }
+  }
+
+  const handleUsernameInput = (e) => {
+    console.log("usernameinput 변경시작")
+    setUsername(e.target.value);
+    console.log("usernameinput 변경끝")
+  }
+
+  const handleEmailInput = (e) => {
+    const errors = {}
+    const valid = {}
+    setEmail(e.target.value);
+    validateEmail(e.target.value);
+    setValid(valid);
+    setErrors(errors);
+  }
+
+  const handlePasswordInput = (e) => {
+    const errors = {}
+    const valid = {}
+    setPassword(e.target.value);
+    validatePassword(e.target.value);
+    setValid(valid);
+    setErrors(errors);
+  }
+
+  useEffect(() => {
+    console.log("usernameinput useEffect시작")
+    const newErrors = {}
+    const newValid = {}
+    validateUsername(username);
+    setValid({...newValid});
+    setErrors({...newErrors});
+    console.log(valid);
+    console.log("usernameinput useEffect변경끝")
+  
+  }, [username])
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     dispatch({type:"UPDATE_START"});
-    // const validateUsername = (username) => {
-    //   const testValidLength = 1 <= username.length && username.length <= 12;
-    //   if(testValidLength) {
-    //     const testValidChar = /^([a-Zㄱ-ㅎㅏ-ㅣ가-힣0-9])$/.test(username)
-    //     if(testValidChar) {
-    //       return (true);
-    //     } else {
-    //       alert("특수문자를 제외한 한글,영문,숫자만 가능합니다");
-    //       return (false);
-    //     }
-    //   } else {
-    //     alert("유저명은 1자 이상 12자 이하로 정해주세요");
-    //     return (false);
-    //   }
-    // };
-
-    const errors = {}
-    const valid = {}
-
-    const validateUsername = (username) => {
-      const isLengthValid = /^[A-Za-z\d@$!%*?&]{1,12}$/.test(username);
-      const isLetterValid = /^[a-zA-Z\u3131-\u318E\uAC00-\uD7A3\d]*$/.test(username);
-      switch (false) {
-        case isLengthValid:
-          valid["name"] = false;
-          errors["name"] = "이름은 1자 이상 12자 이하로 해주세요";
-          break;
-        
-        case isLetterValid:
-          valid["name"] = false;
-          errors["name"] = "이름은 한글,영문,숫자만 사용가능합니다.";
-          break;
-      
-        default:
-          valid["name"] = true;
-          valid["message"] = "사용가능한 닉네임입니다.";
-          break;
-      }
-    }
-
-    const validatePassword = (password) => {
-      const islengthValid = /^[A-Za-z\d@$!%*?&]{3,20}$/.test(password);
-      const atLeastOneUpperLowercaseLetter = /^(?=.*[a-z])(?=.*[A-Z])$/.test(password);
-      const atLeastOneNumber = /^(?=.*\d)$/.test(password);
-      const atLeastOneSpecialCharacter = /^(?=.*[@$!%*?&])$/.test(password);
-      switch (false) {
-        case islengthValid:
-          valid["password"] = false;
-          errors["password"] = "비밀번호는 3자 이상 20자 이하만 가능합니다."
-          break;
-        
-        case atLeastOneUpperLowercaseLetter:
-          valid["password"] = false;
-          errors["password"] = "영문 소문자,대문자가 각각 1개 이상 포함되어야 합니다."
-          break;
-        
-        case atLeastOneNumber:
-          valid["password"] = false;
-          errors["password"] = "숫자가 1개 이상 포함되어야 합니다."
-          break;
-        
-        case atLeastOneSpecialCharacter:
-          valid["password"] = false;
-          errors["password"] = "특수문자가 1개 이상 포함되어야 합니다."
-          break;
-      
-        default:
-          valid["password"] = true;
-          valid["message"] = "사용가능한 비밀번호입니다."
-          break;
-      }
-    }
-
-    // const validateEmail = (email) => {
-    //   const isEmailFormat = 
-    // }
-
-    const updatedUser = {
-      userId: user._id,
-      username,
-      email,
-      password,
-      biography: bio,
-    };
-    if(file) {
-      const formData = new FormData();
-      const filename = Date.now() + file.name;
-      formData.append("name",filename);
-      formData.append("file",file);
-      updatedUser.profileImg = filename;
-      console.log(updatedUser,"updateprofile")
-      for (let value of formData.values()){
-        console.log(value);
+    if(valid.name && valid.password && valid.email) {
+      const updatedUser = {
+        userId: user._id,
+        username,
+        email,
+        password,
+        biography: bio,
       };
-      try {
-        console.log("trying upload")
-        await axios.post("http://localhost:5000/api/upload", formData);
-      } catch (err) {
-        console.dir(err,"multer업로드중 에러");
+      if(file) {
+        const formData = new FormData();
+        const filename = Date.now() + file.name;
+        formData.append("name",filename);
+        formData.append("file",file);
+        updatedUser.profileImg = filename;
+        console.log(updatedUser,"updateprofile")
+        for (let value of formData.values()){
+          console.log(value);
+        };
+        try {
+          console.log("trying upload")
+          await axios.post("http://localhost:5000/api/upload", formData);
+        } catch (err) {
+          console.dir(err,"multer업로드중 에러");
+        }
       }
-    }
-    try {
-      const res = await axios.put(`http://localhost:5000/api/user/${user._id}`, updatedUser);
-      setSuccess(true);
-      dispatch({type:"UPDATE_SUCCESS", payload: res.data });
-    } catch (err) {
-      console.dir(err,"서버업로드중에러");
+      try {
+        const res = await axios.put(`http://localhost:5000/api/user/${user._id}`, updatedUser);
+        setSuccess(true);
+        dispatch({type:"UPDATE_SUCCESS", payload: res.data });
+      } catch (err) {
+        console.dir(err,"서버업로드중에러");
+        dispatch({type:"UPDATE_FAILURE"});
+      }
+    } else {
+      console.log("유효성 검사 통과실패");
       dispatch({type:"UPDATE_FAILURE"});
     }
   };
 
+  console.log("before return",valid.name,errors);
   return (
     <div className="settings">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <div className="settings-wrapper">
           <div className="settings-title">
             <span className="settings-update-title">내 정보 수정하기</span>
@@ -154,14 +189,15 @@ export default function Settings() {
             </div>
             <div className="settings-profile-wrap">
               <label htmlFor="">사용자 이름</label>
-              <input type="text" placeholder={user.username} onChange={(e) => setUsername(e.target.value)}/>
-              <span className="settings-error">{errors["name"]}</span>
+              <input type="text" placeholder={user.username} onChange={handleUsernameInput}/>
+              <span>abc {valid["name"]}{valid.message}{errors.name}</span>
+              {/* {errors && <span className="settings-error">{errors["name"]}</span> } */}
               <label htmlFor="">이메일</label>
-              <input type="email" placeholder={user.email} onChange={(e) => setEmail(e.target.value)}/>
-              <span className="settings-error">{errors["email"]}</span>
+              <input type="email" placeholder={user.email} onChange={handleEmailInput}/>
+              {/* {errors && <span className="settings-error">{errors["email"]}</span> } */}
               <label htmlFor="">비밀번호</label>
-              <input type="password" onChange={(e) => setPassword(e.target.value)}/>
-              <span className="settings-error">{errors["password"]}</span>
+              <input type="password" onChange={handlePasswordInput}/>
+              {/* <span className="settings-error">{errors["password"]}</span> */}
             </div>
             <label htmlFor="settings-bio">자기소개</label>
             <textarea onChange={(e) => setBio(e.target.value)} cols="30" rows="10" className="settings-bio"></textarea>
