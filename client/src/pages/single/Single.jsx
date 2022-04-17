efb0bf87367c3844d32e85b8d5bbb88e017f32d3
iwipwq <iwipwq@gmail.com>
2022-04-17 17:56:09 +0900

    fix sidebar crash

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -1,12 +1,15 @@
+import { useContext } from "react"
 import Sidebar from "../../components/sidebar/Sidebar"
 import SinglePost from "../../components/singlePost/SinglePost"
+import { Context } from "../../context/Context"
 import "./single.scss"
 
 export default function Single() {
+  const { user } = useContext(Context);
   return (
     <div className="single-wrapper">
         <div className="single">
-            <Sidebar />
+            { user && <Sidebar />}
             <SinglePost />
         </div>
     </div>
