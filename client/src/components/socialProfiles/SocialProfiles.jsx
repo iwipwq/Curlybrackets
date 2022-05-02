199770af18180eb9ef422e57ab2c88f0c94defaf
iwipwq <iwipwq@gmail.com>
2022-05-02 23:12:34 +0900

    update- useRef in  mapping element

--- client/src/components/socialProfiles/SocialProfiles.jsx
+++ client/src/components/socialProfiles/SocialProfiles.jsx
@@ -12,42 +12,44 @@ let renderTime = 0;
 
 export default function SocialProfiles() {
   const [socials, setSocials] = useState([]);
+  const socialUrlRefs = useRef([]);
   // const containerRef = useRef();
   // const listRef = useRef();
 
   useEffect(() => {
-    const resetSocial = () => {
-      const newSocials = [...user.socials];
-      const tmpSocial = [];
-      newSocials.map((item) =>
-        tmpSocial.push({
+    const getSocial = () => {
+      const $socials = [...user.socials];
+      const socialsWithEditmode = [];
+      $socials.map((item) =>
+      socialsWithEditmode.push({
           name: item.name,
           url: item.url,
           editmode: false
         })
+        //setSocials([...user.socials,name:itme])
       );
-      setSocials(tmpSocial);
+      setSocials(socialsWithEditmode);
     };
-    resetSocial();
+    getSocial();
   }, []);
 
-  const handleOnSubmit = (name, e) => {
+  const handleOnSubmit = (name, index, e) => {
     e.preventDefault();
     // const ulElem = containerRef.current.childNodes;
     // console.log(Array.from(ulElem));
-    const socialsCopied = [...socials];
-    const objToInverse = socialsCopied.find((obj) => obj.name === name);
-    objToInverse.editmode = !objToInverse.editmode;
-    setSocials(socialsCopied);
+    console.log(socialUrlRefs.current[index]?.value);
+    const $socials = [...socials];
+    const currentElementState = $socials.find((obj) => obj.name === name);
+    currentElementState.editmode = !currentElementState.editmode;
+    setSocials($socials);
     console.log(name, e.target);
   };
 
-  const inputGenerator = socials.map((social) => 
-    social.editmode ?
-      (<li key={social.name} className="input-list">
+  const socialItemEditmode = (social, index) => (
+    <li key={social.name} className="input-list">
       <form
         className={`social-form ${social.name}`}
-        onSubmit={(e) => handleOnSubmit(social.name, e)}
+        onSubmit={(e) => handleOnSubmit(social.name, index, e)}
       >
         <label className="social-input-label" htmlFor="social-input">
           {social.name}
@@ -57,18 +59,26 @@ export default function SocialProfiles() {
           className="social-input"
           type="text"
           defaultValue={social.url}
+          ref={currentRef => socialUrlRefs[index] = currentRef}
         />
         <button className="submit-button" type="submit">
           change
         </button>
       </form>
-    </li>)
-    : <li key={social.name}>
+    </li>
+  )
+
+  const socialItem = (social) => (
+    <li key={social.name}>
       <div>
       {social.name} : {social.url}
       <button onClick={(e)=>handleOnSubmit(social.name,e)}>change</button>
       </div>
     </li>
+  )
+
+  const renderSocialList = socials.map((social, index) => 
+    social.editmode ? socialItemEditmode(social, index) : socialItem(social)
   );
   
     console.log("render+")
@@ -81,7 +91,7 @@ export default function SocialProfiles() {
   return (
     <div className="social">
       <h3>mapping</h3>
-      <ul>{inputGenerator}</ul>
+      <ul>{renderSocialList}</ul>
     </div>
   );
 }
